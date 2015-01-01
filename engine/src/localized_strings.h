9fa92965673d25b274dc9f85dfc024cbc586b95b
Kwasniok <JensKwasniok@web.de>
2015-01-01 21:19:25 +0100

    improved localization: - added support for multiple languages - added 'default' and 'fallback' language - improved error handling

--- engine/src/localized_strings.h
+++ engine/src/localized_strings.h
@@ -17,31 +17,27 @@
 
 namespace cdx {
 
-	// TODO: OPTIMIZE: MAP[const char* --> std::string&] (1st time c_str is read/'translated' Result is saved in map| 2nd time translation is read from map)
-	// TODO: OPTIMIZE: return const char* only
-	// TODO: ADD FALLBACK FOR INCOMPLETE LOCALIZATIONS
-	// TODO?: EXTEND FOR MULTIPLE LANGUAGES (VIA NEW HANDLER CLASS)?
-	class localized_strings {
-		//! the current (global) localization
-		static localized_strings* current;
+	//TODO: EXTENED FOR UNICODE STRINGS!
+	class Localized_String_Map {
 
 		//! maps all ids to the correponding string for the givien language
 		std::map<int, std::string> strings;
 		//! contains the language identifier
 		std::string language_id;
 	public:
-		//! use this method to (re)set the global localization
-		//! @see get()
-		static void set(localized_strings* ls);
-		//! @return pointer to the current global localization
-		static localized_strings* get() {return current;}
+		// pointer value is used for lazy comparison!!
+		//! unique constant to indicate a missed string
+		constexpr static const char* NO_STRING_FOR_ID_MSG = "~NO LOCALIZED STRING~";
 
 		//! adds the vlaue pair (redefinitions are allowed)
 		//! @see get_str(), gett_all()
+		void add_str(const int n, const std::string str);
+		//! adds the vlaue pair (redefinitions are allowed)
+		//! @see get_str(), gett_all()
 		void add_str(std::pair<int, std::string>& isp);
-		//! @return localized string for id and given langauge
+		//! @return localized string for id and given langauge (as a c-string to improve performance)
 		//! @see add_str, get_all()
-		std::string get_str(const int number) const;
+		const char* get_str(const int n) const;
 		//! @return map with all localized strings
 		//! @see add_str(), get_str()
 		const std::map<int, std::string> get_all() const {return strings;}
@@ -52,16 +48,59 @@ namespace cdx {
 		bool set_language_id(const std::string& lang_id);
 		//! @see get_lamguage_id()
 		const std::string get_language_id() const {return language_id;}
+
+		//! clears all information and copies all new information from file until a problem occurs
+		//! @returns true if no problems occurred
+		bool copy_from_file(const std::string& file_path);
 	};
 
-	bool create_localized_strings_from_file(localized_strings& ls, const std::string& file_path);
+	//TODO: ADD REMOVE METHOD (MUST INCLUDE CHECK: IF DEFAULT/FALLBACK LANGUAGE IS REMOVED --> SET POINTER TO NULL)
+	class Localizer {
+		//! the current (global) localization
+		static Localizer* current;
+
+		std::map<std::string, Localized_String_Map> all_languages;
+		Localized_String_Map* default_language;
+		Localized_String_Map* fallback_language;
+
+	public:
+		//! use this method to (re)set the global localizer
+		//! @see get()
+		static void set(Localizer* l) {current = l;}
+		//! @return pointer to the current global localizer
+		static Localizer* get() {return current;}
+
+		//! adds the given strings to a language
+		void add_language(Localized_String_Map& lsm);
+		//! adds the given strings to a language
+		void add_language(Localized_String_Map&& lsm);
+		//! @return a pointer to the associated string set
+		//! (or nullptr if the language id is unknown)
+		Localized_String_Map* get_language(const std::string& lang_id);
+		//! @return true if language is known
+		bool has_language(const std::string& lang_id) const;
+		//! sets the default language
+		//! @return true if langague is known
+		bool set_default_language(const std::string& lang_id);
+		//! sets the fallback language
+		//! @return true if langague is known
+		bool set_fallback_language(const std::string& lang_id);
+
+		//! this function is part of a short cut to improve readability & performance
+		//! @return localized string from default language
+		const char* get_str_from_default_language(const int n) const;
+
+		//! this function is a short cut to improve readability & performance
+		static const char* get_str(const int n);
+
+	};
 
 }
 
 //! FORMAT:\nLANG_ID\nid1, string1;\nid2, string2;\n...
-std::istream& operator>>(std::istream& is, cdx::localized_strings& ls);
+std::istream& operator>>(std::istream& is, cdx::Localized_String_Map& ls);
 //! FORMAT:\nLANG_ID\nid1, string1;\nid2, string2;\n...
-std::ostream& operator<<(std::ostream& os, cdx::localized_strings&  ls);
+std::ostream& operator<<(std::ostream& os, cdx::Localized_String_Map&  ls);
 //! FORMAT:\nid, string
 std::istream& operator>>(std::istream& is, std::pair<int, std::string>& p);
 //! FORMAT:\nid, string
